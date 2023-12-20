c065c41749f35932b1e3d1ef485ba22f12bb6b67
Perahudau Dzianis <tvigger@yandex.ru>
2023-12-20 21:01:31 +0300

    feat: add code for extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let repeatSubStr = '';
+let repeatSubCount = 1;
+const repStr = str;
+let repTime = options.repeatTimes;
+if (!options.repeatTimes) {
+  repTime = 1;
+}
+let repSep = '+';
+let repAdd = '';
+let addRepTime = null;
+let addRepSep = '';
+if (options.separator) {
+  repSep = options.separator;
+}
+if (options.addition) {
+  repAdd = options.addition;
+}
+if (options.additionRepeatTimes) {
+  addRepTime = options.additionRepeatTimes;
+}
+if (options.additionSeparator) {
+  addRepSep = options.additionSeparator;
+}
+/////////
+  function repeatSub(repStr, repTime, repSep, repAdd, addRepTime, addRepSep) {
+    if((repeatSubCount === 1) && (repeatSubCount !== repTime)) {
+      repeatSubStr = repeatSubStr + repStr;
+      repeatSubCount += 1;
+    }
+    if((repeatSubCount === 1) && (repeatSubCount === repTime) && (addRepTime === null)) {
+      repeatSubStr = repeatSubStr + repStr + (repAdd).repeat(1);
+      repeatSubCount += 1;
+    }
+    if((repeatSubCount === 1) && (repeatSubCount === repTime) && (addRepTime !== null)) {
+      repeatSubStr = repeatSubStr + repStr + (repAdd + addRepSep).repeat(addRepTime);
+      repeatSubCount += 1;
+    }
+    if ((repSep !== null) && (repeatSubCount > 2) && (repeatSubCount < repTime)){
+    repeatSubStr = repeatSubStr + (repAdd + addRepSep).repeat(addRepTime) + repSep + repStr;
+    repeatSubCount += 1;
+    }
+    if((repeatSubCount < repTime) && (repTime !== 1)) {
+      repeatSubStr = repeatSubStr + (repAdd + addRepSep).repeat(addRepTime) + repSep + repStr;
+      repeatSubCount += 1;
+      repeatSub(repStr, repTime, repSep, repAdd, addRepTime, addRepSep);
+  }
+  if((repSep !== null) && (repeatSubCount > 2) && (repeatSubCount === repTime)) {
+    repeatSubStr = repeatSubStr + repAdd + repSep + repStr + (repAdd + addRepSep).repeat(addRepTime);
+    repeatSubCount += 1;
+}
+}
+repeatSub(repStr, repTime, repSep, repAdd, addRepTime, addRepSep);
+
+return repeatSubStr;
 }
 
 module.exports = {
